c10a9a43f18b1755971f09cf8e03dcd00979abb0
Holm Smidt <holm.smidt@letsbundle.de>
2021-07-11 14:17:30 -1000

    amplify tutorial

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,82 +1,119 @@
-import Head from 'next/head'
+// pages/index.js
+import { AmplifyAuthenticator } from "@aws-amplify/ui-react";
+import { Amplify, API, Auth, withSSRContext } from "aws-amplify";
+import { GRAPHQL_AUTH_MODE } from '@aws-amplify/api-graphql'
+import Head from "next/head";
+import awsExports from "../src/aws-exports";
+import { createTodo } from "../src/graphql/mutations";
+import { listTodos } from "../src/graphql/queries";
+import styles from "../styles/Home.module.css";
+import { GetServerSideProps } from 'next'
+import React from "react";
+import { CreateTodoMutation, CreateTodoMutationVariables, Todo } from "../src/API";
 
-export default function Home() {
+Amplify.configure({ ...awsExports, ssr: true });
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const SSR = withSSRContext({ req });
+  const response = await SSR.API.graphql({ query: listTodos });
+
+  return {
+    props: {
+      todos: response.data.listTodos.items,
+    },
+  };
+}
+
+const handleCreateTodo = async (event: React.FormEvent<HTMLFormElement>) => {
+  event.preventDefault();
+
+  const form = new FormData(event.currentTarget);
+
+  try {
+		console.log("etaetaet")
+		const variables: CreateTodoMutationVariables = {
+			input: {
+				name: String(form.get("name")),
+				description: String(form.get("description")),
+			}
+		}
+
+    const result = await API.graphql({
+      authMode: GRAPHQL_AUTH_MODE.API_KEY,
+      query: createTodo,
+      variables,
+    });
+
+		console.log(JSON.stringify(result))
+
+		if ('data' in result && result.data ) {
+			const data = result.data as CreateTodoMutation
+			console.log(JSON.stringify(data))
+			window.location.href = `/todos/${data.createTodo.id}`;
+		}
+    
+  } catch ({ errors }) {
+    console.error(...errors);
+    throw new Error(errors[0].message);
+  }
+}
+
+const Home = ({ todos = [] }: { todos: Todo[] }) => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Amplify + Next.js</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+      <main className={styles.main}>
+        <h1 className={styles.title}>Amplify + Next.js</h1>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
+        <p className={styles.description}>
+          <code className={styles.code}>{todos.length}</code>
+          posts
         </p>
 
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
+        <div className={styles.grid}>
+          {todos.map((todo) => (
+            <a className={styles.card} href={`/todos/${todo.id}`} key={todo.id}>
+              <h3>{todo.name}</h3>
+              <p>{todo.description}</p>
+            </a>
+          ))}
 
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
+          <div className={styles.card}>
+            <h3 className={styles.title}>New Todo</h3>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
+            <AmplifyAuthenticator>
+              <form onSubmit={handleCreateTodo}>
+                <fieldset>
+                  <legend>Name</legend>
+                  <input
+                    defaultValue={`Today, ${new Date().toLocaleTimeString()}`}
+                    name="name"
+                  />
+                </fieldset>
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+                <fieldset>
+                  <legend>Description</legend>
+                  <textarea
+                    defaultValue="I have to build an Amplify app with Next.js!"
+                    name="description"
+                  />
+                </fieldset>
+
+                <button>Create Todo</button>
+                <button type="button" onClick={() => Auth.signOut()}>
+                  Sign out
+                </button>
+              </form>
+            </AmplifyAuthenticator>
+          </div>
         </div>
       </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
     </div>
-  )
+  );
 }
+
+export default Home
---
+++ pages/todos/[id].tsx
@@ -0,0 +1,95 @@
+import React from "react";
+import { Amplify, API, Auth, withSSRContext } from "aws-amplify";
+import { GetServerSideProps } from 'next'
+import awsExports from "../../src/aws-exports";
+import { getTodo } from "../../src/graphql/queries";
+import { Todo } from "../../src/API"
+import { PaperClipIcon, ThumbDownIcon } from '@heroicons/react/solid'
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const SSR = withSSRContext(context);
+	const { id } = context.params
+  const response = await SSR.API.graphql({
+		query: getTodo,
+		variables: { id }
+	});
+
+	console.log(JSON.stringify(response))
+  return {
+    props: {
+      todo: response.data.getTodo,
+    },
+  };
+}
+
+export default function TodoConponent ({
+	todo
+} : {
+	todo: Todo 
+}) {
+  return (
+    <div className="bg-white shadow overflow-hidden sm:rounded-lg">
+      <div className="px-4 py-5 sm:px-6">
+        <h3 className="text-lg leading-6 font-medium text-gray-900">Todo ID</h3>
+        <p className="mt-1 max-w-2xl text-sm text-gray-500">{ todo.id }</p>
+      </div>
+      <div className="border-t border-gray-200 px-4 py-5 sm:p-0">
+        <dl className="sm:divide-y sm:divide-gray-200">
+          <div className="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt className="text-sm font-medium text-gray-500">Name</dt>
+            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{todo.name}</dd>
+          </div>
+          <div className="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt className="text-sm font-medium text-gray-500">Description</dt>
+            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{ todo.description }</dd>
+          </div>
+          <div className="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt className="text-sm font-medium text-gray-500">Created at</dt>
+            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{ todo.createdAt }</dd>
+          </div>
+          <div className="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt className="text-sm font-medium text-gray-500">Updated at </dt>
+            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{ todo.updatedAt }</dd>
+          </div>
+          <div className="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt className="text-sm font-medium text-gray-500">About</dt>
+            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+              Fugiat ipsum ipsum deserunt culpa aute sint do nostrud anim incididunt cillum culpa consequat. Excepteur
+              qui ipsum aliquip consequat sint. Sit id mollit nulla mollit nostrud in ea officia proident. Irure nostrud
+              pariatur mollit ad adipisicing reprehenderit deserunt qui eu.
+            </dd>
+          </div>
+          <div className="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt className="text-sm font-medium text-gray-500">Attachments</dt>
+            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+              <ul className="border border-gray-200 rounded-md divide-y divide-gray-200">
+                <li className="pl-3 pr-4 py-3 flex items-center justify-between text-sm">
+                  <div className="w-0 flex-1 flex items-center">
+                    <PaperClipIcon className="flex-shrink-0 h-5 w-5 text-gray-400" aria-hidden="true" />
+                    <span className="ml-2 flex-1 w-0 truncate">resume_back_end_developer.pdf</span>
+                  </div>
+                  <div className="ml-4 flex-shrink-0">
+                    <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
+                      Download
+                    </a>
+                  </div>
+                </li>
+                <li className="pl-3 pr-4 py-3 flex items-center justify-between text-sm">
+                  <div className="w-0 flex-1 flex items-center">
+                    <PaperClipIcon className="flex-shrink-0 h-5 w-5 text-gray-400" aria-hidden="true" />
+                    <span className="ml-2 flex-1 w-0 truncate">coverletter_back_end_developer.pdf</span>
+                  </div>
+                  <div className="ml-4 flex-shrink-0">
+                    <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
+                      Download
+                    </a>
+                  </div>
+                </li>
+              </ul>
+            </dd>
+          </div>
+        </dl>
+      </div>
+    </div>
+  )
+}
